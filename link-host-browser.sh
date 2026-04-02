cb6a03dbd59b862ea593cfdca93abd3293c1ec57
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-02 21:27:38 +0100

    docs: add stackoverflow where I got the link host browser answer from

--- link-host-browser.sh
+++ link-host-browser.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# https://stackoverflow.com/questions/54437534/docker-open-a-url-in-the-host-browser/73750272#73750272
 [ ! -p hostbrowserpipe ] && mkfifo hostbrowserpipe
 while read -r URL < hostbrowserpipe; do xdg-open "$URL"; done
